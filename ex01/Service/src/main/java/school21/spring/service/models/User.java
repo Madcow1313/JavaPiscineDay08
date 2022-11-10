9e056f956e61c7882e2dfd2ed5a0935292d6047e
wabathur <safbul@gmail.com>
2022-11-10 16:40:39 +0300

    ex01 start

---
+++ ex01/Service/src/main/java/school21/spring/service/models/User.java
@@ -0,0 +1,6 @@
+package school21.spring.service.models;
+
+public class User {
+    Long identifier;
+    String email;
+}
---
+++ ex01/Service/src/main/java/school21/spring/service/repositories/UsersRepositoryJdbcImpl.java
@@ -0,0 +1,39 @@
+package school21.spring.service.repositories;
+
+import school21.spring.service.models.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public class UsersRepositoryJdbcImpl implements UsersRepository {
+
+    @Override
+    public User findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<User> findAll() {
+        return null;
+    }
+
+    @Override
+    public void save(User entity) {
+
+    }
+
+    @Override
+    public void update(User entity) {
+
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+
+    @Override
+    public Optional<User> findByEmail(String email) {
+        return Optional.empty();
+    }
+}
